c2e4e3e35bba69e6cf7cef8e7212423baf0ad177
Eugene Koretsky <E.Koretsky@outlook.com>
2021-01-16 21:21:14 +0300

    Добавлен файл cli_constants

---
+++ src/service/cli/cli_constants.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports.DEFAULT_COMMAND = `--help`;
+module.exports.DEFAULT_COUNT_PUBLICATIONS = 1;
+module.exports.FILE_NAME = `mocks.json`;
+module.exports.COUNT_LAST_MONTH = 3;
+
+module.exports.USER_ARGV_INDEX = 2;
+module.exports.EXIT_CODE = {
+  error: 1,
+  success: 0,
+};
