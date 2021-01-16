247c27a09ae8ec123db6e6ec31e9d99ef1a3fa54
Eugene Koretsky <E.Koretsky@outlook.com>
2021-01-16 21:12:39 +0300

    рефакторинг utils.js

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,7 @@
  * @return {number}
  */
 
-module.exports.getRandomInt = (min, max) => {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -18,13 +18,40 @@ module.exports.getRandomInt = (min, max) => {
  * @function
  * @name shuffle
  * @param {Array} someArray
+ * @return {Array}
  */
 
-module.exports.shuffle = (someArray) => {
+const shuffle = (someArray) => {
   for (let i = someArray.length - 1; i > 0; i--) {
     const randomPosition = Math.floor(Math.random() * i);
     [someArray[i], someArray[randomPosition]] = [someArray[randomPosition], someArray[i]];
   }
   return someArray;
+};
+
+/**
+ * @param {number} countLastMonth
+ * @param {Date} [endRangeDate]
+ * @return {Date}
+ */
+
+const getRandomDate = (countLastMonth = 1, endRangeDate = new Date()) => {
+  const endMonth = endRangeDate.getUTCMonth();
+
+  const day = endRangeDate.getUTCDate();
+
+  const startDateRange = new Date(Date.UTC(endRangeDate.getUTCFullYear(), endMonth - countLastMonth, day));
+
+  const startAsMilliseconds = startDateRange.getTime();
+  const endAsMilliseconds = endRangeDate.getTime();
+
+  const dateAsMilliseconds = getRandomInt(startAsMilliseconds, endAsMilliseconds);
+  return new Date(dateAsMilliseconds);
+};
+
+module.exports = {
+  getRandomInt,
+  shuffle,
+  getRandomDate,
 };
 
