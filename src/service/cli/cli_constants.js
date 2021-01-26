9bc66459793bc80bb397d46aaf6cdd6a34f9a001
Eugene Koretsky <E.Koretsky@outlook.com>
2021-01-26 13:16:53 +0300

    Добавил пути для текстовых файлов в константы

--- src/service/cli/cli_constants.js
+++ src/service/cli/cli_constants.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const path = require(`path`);
+
 module.exports.DEFAULT_COMMAND = `--help`;
 module.exports.DEFAULT_COUNT_PUBLICATIONS = 1;
 module.exports.FILE_NAME = `mocks.json`;
@@ -9,4 +11,10 @@ module.exports.USER_ARGV_INDEX = 2;
 module.exports.EXIT_CODE = {
   error: 1,
   success: 0,
+};
+
+module.exports.PATH_TO_DATA = {
+  TITLES: path.join(process.env.NODE_PATH, `data`, `titles.txt`),
+  CATEGORIES: path.join(process.env.NODE_PATH, `data`, `categories.txt`),
+  SENTENCES: path.join(process.env.NODE_PATH, `data`, `sentences.txt`)
 };
