3132e73430c8100bec1f7f1b29b743774437e6f3
Eugene Koretsky <E.Koretsky@outlook.com>
2021-01-29 02:22:33 +0300

    Рефакторинг cli_constants.js. Добавлена константа для порта по умолчанию

--- src/service/cli/cli_constants.js
+++ src/service/cli/cli_constants.js
@@ -2,18 +2,19 @@
 
 const path = require(`path`);
 
-module.exports.DEFAULT_COMMAND = `--help`;
-module.exports.DEFAULT_COUNT_PUBLICATIONS = 1;
-module.exports.FILE_NAME = `mocks.json`;
-module.exports.COUNT_LAST_MONTH = 3;
+exports.DEFAULT_COMMAND = `--help`;
+exports.DEFAULT_COUNT_PUBLICATIONS = 1;
+exports.FILE_NAME = `mocks.json`;
+exports.COUNT_LAST_MONTH = 3;
+exports.DEFAULT_PORT = 3000;
 
-module.exports.USER_ARGV_INDEX = 2;
-module.exports.EXIT_CODE = {
+exports.USER_ARGV_INDEX = 2;
+exports.EXIT_CODE = {
   error: 1,
   success: 0,
 };
 
-module.exports.PATH_TO_DATA = {
+exports.PATH_TO_DATA = {
   TITLES: path.join(process.env.NODE_PATH, `data`, `titles.txt`),
   CATEGORIES: path.join(process.env.NODE_PATH, `data`, `categories.txt`),
   SENTENCES: path.join(process.env.NODE_PATH, `data`, `sentences.txt`)
