e084088aa5e881de90736263c73abfd56037b65c
Eugene Koretsky <E.Koretsky@outlook.com>
2021-02-01 02:28:24 +0300

    Подключен модуль с роутами

--- src/express/index.js
+++ src/express/index.js
@@ -1,8 +1,11 @@
 "use strict";
 
 const express = require(`express`);
+const routes = require(`./routes/index`);
 
 const DEFAULT_PORT = 8080;
 const app = express();
+
+app.use(`/`, routes.mainRoutes);
 
 app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
