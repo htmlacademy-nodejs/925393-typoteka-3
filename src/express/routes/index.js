af76ef7ea8c42afa9d168ac55dc40d1d1a43b790
Eugene Koretsky <E.Koretsky@outlook.com>
2021-02-01 03:17:35 +0300

    Добавлен роуты в index, экспортирующий их наружу

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -1,7 +1,11 @@
 "use strict";
 
 const mainRoutes = require(`./main-routes`);
+const myRoutes = require(`./my-routes`);
+const articleRoutes = require(`./article-routes`);
 
 module.exports = {
   mainRoutes,
+  myRoutes,
+  articleRoutes
 };
