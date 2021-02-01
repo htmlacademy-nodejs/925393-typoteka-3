5a85b36aa647df7e5c6a1d1fed2d066782af4ebb
Eugene Koretsky <E.Koretsky@outlook.com>
2021-02-01 03:16:46 +0300

    Добавлен маршруты в экспресс

--- src/express/index.js
+++ src/express/index.js
@@ -7,5 +7,7 @@ const DEFAULT_PORT = 8080;
 const app = express();
 
 app.use(`/`, routes.mainRoutes);
+app.use(`/my`, routes.myRoutes);
+app.use(`/article`, routes.articleRoutes);
 
 app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
