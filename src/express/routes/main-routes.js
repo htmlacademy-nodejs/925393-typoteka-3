ea5488c360c7e1b5c759a15803b424202966d245
Eugene Koretsky <E.Koretsky@outlook.com>
2021-02-01 03:15:59 +0300

    Добавлен новый маршрут в основные роуты

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -4,9 +4,10 @@ const {Router} = require(`express`);
 
 const mainRoutes = new Router();
 
-mainRoutes.get(`/`, (req, res) => console.log(`main`));
+mainRoutes.get(`/`, (req, res) => console.log(`/`));
 mainRoutes.get(`/login`, (req, res) => console.log(`/login`));
 mainRoutes.get(`/register`, (req, res) => console.log(`/register`));
 mainRoutes.get(`/search`, (req, res) => console.log(`/search`));
+mainRoutes.get(`/category`, (req, res) => console.log(`/category`));
 
 module.exports = mainRoutes;
