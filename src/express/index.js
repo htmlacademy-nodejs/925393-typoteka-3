a389c902c85e512db335f85871792a1b28b39230
Eugene Koretsky <E.Koretsky@outlook.com>
2021-02-17 02:14:09 +0300

    Рефакторинг роутов

--- src/express/index.js
+++ src/express/index.js
@@ -2,12 +2,44 @@
 
 const express = require(`express`);
 const routes = require(`./routes/index`);
+const expressHandlebars = require(`express-handlebars`);
+const path = require(`path`);
+const {StatusCodes} = require(`http-status-codes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = path.resolve(__dirname, `public`);
+
+// config handlebars engine
+const ENGINE_CONFIG = {
+  layoutsDir: `templates/layouts`,
+  defaultLayout: `default-layout`,
+  extname: `.hbs`,
+};
+
 const app = express();
+
+// middleware
+app.use(express.static(PUBLIC_DIR));
+
+app.engine(`hbs`, expressHandlebars(ENGINE_CONFIG));
+app.set(`views`, `templates`);
 
 app.use(`/`, routes.mainRoutes);
 app.use(`/my`, routes.myRoutes);
 app.use(`/article`, routes.articleRoutes);
+
+app.use((req, res) => {
+  res.status(StatusCodes.NOT_FOUND).render(`pages/404.hbs`, {
+    layout: `blue-color-layout`,
+    codeError: StatusCodes.NOT_FOUND
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).render(`pages/500.hbs`, {
+    layout: `blue-color-layout`,
+    codeError: StatusCodes.INTERNAL_SERVER_ERROR
+  });
+});
 
 app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
--- src/express/routes/article-routes.js
+++ src/express/routes/article-routes.js
@@ -4,9 +4,17 @@ const {Router} = require(`express`);
 
 const articleRoutes = new Router();
 
-articleRoutes.get(`/add`, (req, res) => console.log(`/article/add`));
+articleRoutes.get(`/add`, (req, res) => {
+  res.render(`pages/new-post.hbs`);
+});
 articleRoutes.get(`/edit/:id`, (req, res) => console.log(`/articles/edit/:id`));
-articleRoutes.get(`/category/:id`, (req, res) => console.log(`/articles/category/:id`));
-articleRoutes.get(`/:id`, (req, res) => console.log(`/articles/:id`));
+
+articleRoutes.get(`/category/:id`, (req, res) => {
+  res.render(`pages/articles-by-category.hbs`);
+});
+
+articleRoutes.get(`/:id`, (req, res) => {
+  res.render(`pages/post.hbs`);
+});
 
 module.exports = articleRoutes;
