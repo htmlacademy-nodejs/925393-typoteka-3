702c7f2e50268b0560dc88a7b195bd1b802289fb
E.Koretsky <E.Koretsky@outlook.com>
2021-07-05 00:47:38 +0300

    Добавлен файл с логикой чтения файла

---
+++ src/service/lib/get_mock_data.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const fs = require(`fs`).promises;
+const {FILE_NAME} = require(`../constants`);
+
+let data = null;
+
+const getMockData = async () => {
+  if (data !== null) {
+    return Promise.resolve(data);
+  }
+
+  try {
+    const fileContent = await fs.readFile(FILE_NAME);
+    data = JSON.parse(`${fileContent}`);
+  } catch (err) {
+    console.error(err);
+    return Promise.reject(err);
+  }
+
+  return Promise.resolve(data);
+};
+
+module.exports = getMockData;
