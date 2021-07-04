91b92e8b3f07d10eee205432ce1c7db239760aba
E.Koretsky <E.Koretsky@outlook.com>
2021-07-05 00:46:44 +0300

    Добавлена константа API_PREFIX

--- src/service/constants.js
+++ src/service/constants.js
@@ -9,6 +9,7 @@ exports.COUNT_LAST_MONTH = 3;
 exports.DEFAULT_PORT = 3000;
 exports.MAX_ID_LENGTH = 6;
 exports.MAX_COMMENTS = 4;
+exports.API_PREFIX = `/api`;
 
 exports.USER_ARGV_INDEX = 2;
 exports.EXIT_CODE = {
