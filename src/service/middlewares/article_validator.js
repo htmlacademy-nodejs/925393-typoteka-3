d8e25fe99538caf454f903554af849cc0e7c01d7
E.Koretsky <E.Koretsky@outlook.com>
2021-07-06 16:27:42 +0300

    рефакторинг валидатора для статьи

--- src/service/middlewares/article_validator.js
+++ src/service/middlewares/article_validator.js
@@ -1,18 +1,25 @@
 "use strict";
 
-const {StatusCodes} = require(`http-status-codes`);
+const {StatusCodes, getReasonPhrase} = require(`http-status-codes`);
 const {ARTICLE_KEYS} = require(`./middlewares_constants`);
 
 module.exports = (req, res, next) => {
-  const newArticle = req.body;
-  const keys = Object.keys(newArticle);
-  const keysExists = ARTICLE_KEYS.every((key) => keys.includes(key));
+  try {
+    const newArticle = req.body;
+    const keys = Object.keys(newArticle);
+    const keysExists = ARTICLE_KEYS.every((key) => keys.includes(key));
 
-  if (!keysExists) {
+    if (!keysExists) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json(`Bad request: В объекте запроса определены не все поля`);
+    }
+
+    next();
+  } catch (e) {
     res
-      .status(StatusCodes.BAD_REQUEST)
-      .json(`Bad request: В объекте запроса определены не все поля`);
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR));
+    console.error(e);
   }
-
-  next();
 };
