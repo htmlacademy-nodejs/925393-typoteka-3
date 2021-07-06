d9dc3e1832a12f3f849a34accd00f8d2e549ce14
E.Koretsky <E.Koretsky@outlook.com>
2021-07-06 16:28:01 +0300

    рефакторинг контроллера для статьи

--- src/service/controllers/articles_controller.js
+++ src/service/controllers/articles_controller.js
@@ -9,28 +9,44 @@ const {MAX_ID_LENGTH} = require(`../constants`);
 class ArticlesController {
   constructor(data) {
     this._data = data;
+    this.getAll = this.getAll.bind(this);
+    this.getOne = this.getOne.bind(this);
+    this.create = this.create.bind(this);
+    this.update = this.update.bind(this);
+    this.drop = this.drop.bind(this);
   }
 
   getAll(req, res) {
-    res
-      .status(StatusCodes.OK)
-      .json(this._data);
-  }
-
-  getOne(req, res) {
     try {
-      const {articleId} = req.params;
-      const article = this._data.find((item) => item.id === articleId);
-      if (!article) {
-        sendResponseWithError(res, StatusCodes.NOT_FOUND);
-      }
       res
         .status(StatusCodes.OK)
-        .json(article);
+        .json(this._data);
     } catch (e) {
       sendResponseWithError(res);
       console.error(e);
     }
+  }
+
+  getOne(req, res) {
+    let article;
+    try {
+      const {articleId} = req.params;
+      const {isNotResponse} = res.locals;
+      article = this._data.find((item) => item.id === articleId);
+
+      if (!article) {
+        sendResponseWithError(res, StatusCodes.NOT_FOUND);
+      }
+      if (!isNotResponse) {
+        res
+          .status(StatusCodes.OK)
+          .json(article);
+      }
+    } catch (e) {
+      sendResponseWithError(res);
+      console.error(e);
+    }
+    return article;
   }
 
   create(req, res) {
@@ -38,12 +54,46 @@ class ArticlesController {
       const newData = Object.assign({
         id: nanoid(MAX_ID_LENGTH),
       }, req.body, {
-        comments: []
+        comments: [],
       });
+      this._data.push(newData);
 
       res
         .status(StatusCodes.CREATED)
         .json(newData);
+    } catch (e) {
+      sendResponseWithError(res);
+      console.error(e);
+    }
+  }
+
+  update(req, res) {
+    try {
+      const {articleId} = req.params;
+      const oldArticle = this._data.find((item) => item.id === articleId);
+
+      const updatedArticle = Object.assign(oldArticle, req.body);
+      res
+        .status(StatusCodes.OK)
+        .json(updatedArticle);
+    } catch (e) {
+      sendResponseWithError(res);
+      console.error(e);
+    }
+  }
+
+  drop(req, res) {
+    try {
+      const {articleId} = req.params;
+      const article = this._data.find((item) => item.id === articleId);
+
+      if (!article) {
+        sendResponseWithError(res, StatusCodes.NOT_FOUND);
+      }
+      this._data = this._data.filter((item) => item.id !== articleId);
+      res
+        .status(StatusCodes.OK)
+        .json(article);
     } catch (e) {
       sendResponseWithError(res);
       console.error(e);
