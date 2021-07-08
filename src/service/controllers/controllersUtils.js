4ca3ecfd703342438801cfe040028184aa28e999
E.Koretsky <E.Koretsky@outlook.com>
2021-07-08 16:51:11 +0300

    Рефакторинг функции обработки и отправки сообщений об ошибках. Добавлена возможность добавить свое сообщение

--- src/service/controllers/controllersUtils.js
+++ src/service/controllers/controllersUtils.js
@@ -2,10 +2,11 @@
 
 const {StatusCodes, getReasonPhrase} = require(`http-status-codes`);
 
-const sendResponseWithError = (res, statusCode = StatusCodes.INTERNAL_SERVER_ERROR) => {
+const sendResponseWithError = (res, statusCode = StatusCodes.INTERNAL_SERVER_ERROR, customMessage = ``) => {
+  const message = customMessage ? `: ${customMessage}` : customMessage;
   res
     .status(statusCode)
-    .json(getReasonPhrase(statusCode));
+    .json(`${getReasonPhrase(statusCode)}${message}`);
 };
 
 module.exports = {
