ac838a610978e20e068ce69d5410f18a85f7fe07
E.Koretsky <E.Koretsky@outlook.com>
2021-07-23 17:05:54 +0300

    Добавлены тесты для комментариев

---
+++ src/service/middlewares/comment_exists.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const {StatusCodes} = require(`http-status-codes`);
+const {sendResponseWithError} = require(`../controllers/controllersUtils`);
+
+module.exports = (req, res, next) => {
+  try {
+    const {article} = res.locals;
+    const {commentId} = req.params;
+
+    const foundComment = article.comments.find((comment) => comment.id === commentId);
+
+    if (!foundComment) {
+      sendResponseWithError(res, StatusCodes.NOT_FOUND);
+    }
+    res.locals.foundComment = foundComment;
+
+    next();
+  } catch (e) {
+    sendResponseWithError(res);
+    console.error(e);
+  }
+  return null;
+};
