22be7e9ae12ad28519fc6f4938a89ec97bd0131d
E.Koretsky <E.Koretsky@outlook.com>
2021-07-23 17:04:30 +0300

    ФИкс articles_routes.js в АПИ

--- src/service/api/articles_routes.js
+++ src/service/api/articles_routes.js
@@ -4,11 +4,13 @@ const Router = require(`express`);
 
 const articleValidator = require(`../middlewares/article_validator`);
 const articleExist = require(`../middlewares/article_exist`);
+const commentExist = require(`../middlewares/comment_exists`);
 const commentsValidator = require(`../middlewares/comments_validator`);
 
-const articleRouter = new Router();
 
 module.exports = function (app, mainController, ...args) {
+  const articleRouter = new Router();
+
   const [commentsController] = args;
   app.use(`/articles`, articleRouter);
 
@@ -16,12 +18,12 @@ module.exports = function (app, mainController, ...args) {
   articleRouter.post(`/`, articleValidator, mainController.create);
 
   articleRouter.get(`/:articleId`, mainController.getOne);
-  articleRouter.put(`/:articleId`, articleValidator, mainController.update);
-  articleRouter.delete(`/:articleId`, mainController.drop);
+  articleRouter.put(`/:articleId`, [articleExist(mainController), articleValidator], mainController.update);
+  articleRouter.delete(`/:articleId`, articleExist(mainController), mainController.drop);
 
   articleRouter.get(`/:articleId/comments`, articleExist(mainController), commentsController.getAll);
   articleRouter.post(`/:articleId/comments`, [articleExist(mainController), commentsValidator], commentsController.create);
-  articleRouter.delete(`/:articleId/comments/:commentId`, articleExist(mainController), commentsController.drop);
+  articleRouter.delete(`/:articleId/comments/:commentId`, [articleExist(mainController), commentExist], commentsController.drop);
 };
 
 
