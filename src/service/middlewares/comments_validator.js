bc7c36cfe6898a64e5191e2945edeb501622b52c
E.Koretsky <E.Koretsky@outlook.com>
2021-07-25 00:19:48 +0300

    в comments_validator.js добавлен try/catch

--- src/service/middlewares/comments_validator.js
+++ src/service/middlewares/comments_validator.js
@@ -6,14 +6,19 @@ const {COMMENT_KEYS} = require(`./middlewares_constants`);
 const {sendResponseWithError} = require(`../controllers/controllersUtils`);
 
 module.exports = (req, res, next) => {
-  const newComment = req.body;
-  const keys = Object.keys(newComment);
-  const keysExists = COMMENT_KEYS.every((key) => keys.includes(key));
+  try {
+    const newComment = req.body;
+    const keys = Object.keys(newComment);
+    const keysExists = COMMENT_KEYS.every((key) => keys.includes(key));
 
-  if (!keysExists) {
-    return sendResponseWithError(res, StatusCodes.BAD_REQUEST, `В теле запроса определены не все поля!`);
+    if (!keysExists) {
+      return sendResponseWithError(res, StatusCodes.BAD_REQUEST, `В теле запроса определены не все поля!`);
+    }
+
+    next();
+  } catch (e) {
+    sendResponseWithError(res);
+    next(e);
   }
-
-  next();
   return null;
 };
